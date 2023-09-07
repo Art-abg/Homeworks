958889e8724379b0e6da80cfa614e91e4e9626bb
Artur Abgaryan <arty219603@gmail.com>
2023-09-07 18:54:23 +0400

    upload cl ex1

---
+++ Homework4/ClosureExercise1.js
@@ -0,0 +1,7 @@
+//Closure Exercise 1
+
+//Create a counter function using closure that increments a count every time it's called.
+
+const counter = createCounter();
+console.log(counter()); // Output: 1
+console.log(counter()); // Output: 2
