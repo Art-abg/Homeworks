b3a9864f5b852f4e9506a5d082000d85d6bd42b2
Artur Abgaryan <arty219603@gmail.com>
2023-09-07 19:01:22 +0400

    uploaded more exercises

--- Homework4/ClosureExercise1.js
+++ Homework4/ClosureExercise1.js
@@ -3,5 +3,13 @@
 //Create a counter function using closure that increments a count every time it's called.
 
 const counter = createCounter();
+function createCounter() {
+  let count = 0;
+
+  return function increment() {
+    count++;
+    return count;
+  };
+}
 console.log(counter()); // Output: 1
 console.log(counter()); // Output: 2
---
+++ Homework4/ClosureExercise2.js
@@ -0,0 +1,20 @@
+//Closure Ex.2
+
+// Implement a function that returns an object with methods to get and set a private variable
+
+const secretValue = privateVariable(42);
+
+function privateVariable(val) {
+  return {
+    get() {
+      return val;
+    },
+    set(newVal) {
+      val = newVal;
+    }
+  };
+}
+
+console.log(secretValue.get()); // Output: 42
+secretValue.set(100);
+console.log(secretValue.get()); // Output: 100
