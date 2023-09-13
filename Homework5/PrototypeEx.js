08be80ebd219067955908529d4b421bc45a618ab
Artur Abgaryan <arty219603@gmail.com>
2023-09-13 15:11:20 +0400

    completed more exercises

---
+++ Homework5/PrototypeEx.js
@@ -0,0 +1,30 @@
+//Create a draw of prototype inheritance for following code
+
+const arr = [1, 2, 3]; // arr -> Array.prototype -> Object.prototype -> null
+const human = {
+  isHuman: true // human -> Object.prototype -> null
+};
+const str = ""; // str -> String.prototype -> Object.prototype -> null
+const user = {
+  // user -> Object.prototype -> null
+  name: "John",
+  __proto__: human
+};
+const car = {
+  // car -> Object.prototype -> null
+  hasEngine: true
+};
+const mercedes = {
+  // mercedes -> car -> Object.prototype -> null
+  name: "mercedes",
+  __proto__: car
+};
+const bmw = Object.create(null); // bmw -> null
+const emptyObj = {}; // emptyObj -> Object.prototype -> null
+function fn() {
+  // fn -> Function.prototype -> Object.prototype -> null
+  console.log("hello, world!");
+}
+const emptyWithoutProto = Object.create(null); // emptyWithoutProto -> null
+const num = 12; // num -> Number.prototype -> Object.prototype -> null
+const isAuth = false; // isAuth -> Boolean.prototype -> Object.prototype -> nulls
